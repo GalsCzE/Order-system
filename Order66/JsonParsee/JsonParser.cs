b84c57df29c5b7d578d0163b1d8bd7bc147dbdf6
GalsCzE <sevca99@gmail.com>
2017-11-21 12:49:03 +0100

    registration

---
+++ Order66/JsonParsee/JsonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Order66.Interface;
+using Newtonsoft.Json;
+
+namespace Order66.JsonParsee
+{
+    class JsonParser : IParse
+    {
+        public async Task<T> ParseString<T>(string response)
+        {
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(response));
+        }
+    }
+}
--- Order66/Registration.xaml.cs
+++ Order66/Registration.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Order66.Entity;
+using Order66.Framy;
+using RestSharp;
 
 namespace Order66
 {
@@ -20,6 +23,8 @@ namespace Order66
     /// </summary>
     public partial class Registration : Page
     {
+        string url;
+
         public Registration()
         {
             InitializeComponent();
@@ -27,7 +32,33 @@ namespace Order66
 
         private void registr_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                User us = new User();
+                us.Name = registrationName.Text;
+                us.Surname = registrationSurname.Text;
+                us.Login = registrationLogin.Text;
+                us.Password = registrationPassword.Password;
 
+                url = "https://student.sps-prosek.cz/~sevcima14/4ITB/Order-system/Users/Insert.php";
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("cache-control", "no-cache");
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", Newtonsoft.Json.JsonConvert.SerializeObject(us), ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+
+                MessageBox.Show("Úspěšně jste se zaregistroval!");
+
+                registrationName.Text = "";
+                registrationSurname.Text = "";
+                registrationLogin.Text = "";
+                registrationPassword.Password = "";
+            }
+            catch
+            {
+                MessageBox.Show("Vyskytla se chyba");
+            }
         }
 
         private void traceLogin_Click(object sender, RoutedEventArgs e)
