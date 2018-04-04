83b5a24bcd498c00c9c2a53048d17eb02a072dd9
sevcima14 <sevcima14@DST20710>
2018-04-04 11:22:40 +0200

    4.4.

--- Order66/InfoUser.xaml.cs
+++ Order66/InfoUser.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,7 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Order66.Entity;
 using Order66.Framy;
+using Order66.Interface;
+using Order66.JsonParsee;
+using RestSharp;
 
 namespace Order66
 {
@@ -21,15 +27,43 @@ namespace Order66
     /// </summary>
     public partial class InfoUser : Page
     {
+        User u;
         int iid;
 
         public InfoUser(int IDuserss)
         {
             InitializeComponent();
             iid = IDuserss;
+            GETUser();
+            Userjmeno.Text = u.Name;
+            Userprijmeni.Text = u.Surname;
+            Userlog.Text = u.Login;
+            Userheslo.Text = u.Password;
         }
 
-        private void back_Click(object sender, RoutedEventArgs e)
+        private void GETUser()
+        {
+            var client = new RestClient("https://student.sps-prosek.cz/~sevcima14/4ITB/Order-system/Users/dotaz_user.php?ID=" + iid);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
+            HttpStatusCode stat = response.StatusCode;
+            if (stat == HttpStatusCode.OK)
+            {
+                IParse parser = new JsonParser();
+                string result = response.Content.Replace(@"[", "");
+                result = result.Replace(@"]", "");
+                u = JsonConvert.DeserializeObject<User>(result);
+            }
+            else
+            {
+                MessageBox.Show("Špatná stránka!");
+                MessageBox.Show("Nebude to fungovat");
+
+            }
+        }
+
+            private void back_Click(object sender, RoutedEventArgs e)
         {
             BackEnd.frame.Navigate(new Jidelna());
         }
